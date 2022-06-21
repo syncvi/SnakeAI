223de0397b7f3cee7f292f09277ec01be7a9d8ca
Hituh <okazysssie@gmail.com>
2022-06-21 15:42:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from players import *
 from game import *
 
-size = 10
-num_snakes = 1
-players = [RandomPlayer(0)]
-
-gui_size = 800
-pop_size = 100
-num_generations = 500
-num_trails = 1
 window_size = 7
 hidden_size = 15
-board_size = 10
+board_size = 15
+
+pop_size = 100
+num_generations = 1
+num_trails = 1
+
+mutation_chance = 0.1
+mutation_size = 0.1
+iscrossover = True
 
 
 gen_player = GeneticPlayer(pop_size, num_generations, num_trails, window_size, hidden_size, board_size,
-                           mutation_chance=0.3, mutation_size=0.1, iscrossover=True)
+                           mutation_chance, mutation_size, iscrossover)
 gen_player.evolve_pop()
